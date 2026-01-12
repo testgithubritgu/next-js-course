ef2c8f548f1ecbbc889a894ceeabef02796c5be8
testgithubritgu <swapnilraut1698@gmail.com>
2026-01-12 22:10:06 +0530

    feat:added pages in contact folder

---
+++ src/app/contact/new/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function IdContactManager() {
+  return (
+    <div>
+      this is id page
+    </div>
+  )
+}
---
+++ src/app/contact/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewContactPage() {
+  return (
+    <div>
+      this is new contact page
+    </div>
+  )
+}
