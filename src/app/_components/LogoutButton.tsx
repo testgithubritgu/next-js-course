ae32aff911682fb8f293f752a7765b51fbe404df
testgithubritgu <swapnilraut1698@gmail.com>
2026-01-14 18:06:33 +0530

    feat:added useRouter Hook in logout button component

--- src/app/_components/LogoutButton.tsx
+++ src/app/_components/LogoutButton.tsx
@@ -2,10 +2,21 @@
 
 import { Button } from '@/components/ui/button'
 import { logoutAction } from '@/lib/session'
+import { useRouter } from 'next/navigation'
 
 export default function LogoutButton() {
+  const router = useRouter()
   const handelLogout = async () =>{
-    await logoutAction()
+   try {
+     await logoutAction()
+     //The redirect happens in the server action
+     //this client side redirects is as a fallback
+     //redirect("/login")
+    router.push("/login");
+    router.refresh();
+   } catch (error:any) {
+    throw new Error("logout error",error)
+   }
   }
   return (
     <Button onClick={() => handelLogout()}>
